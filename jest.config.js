5f3abe3a2923dd4f8a2a871dd803822ebd33918c
Filip Brebera <breberafilip@icloud.com>
2020-08-09 20:06:17 +0200

    refactor: edited cov path

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
     '.*\\.(vue)$': 'vue-jest',
   },
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/components/**/*.vue'],
+  collectCoverageFrom: ['<rootDir>/src/components/**/*.vue'],
 };
